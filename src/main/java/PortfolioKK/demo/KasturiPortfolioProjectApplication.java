113c3c44be032a8980e183b95a6d8442744542a4
sonali072 <sonalisingla072@gmail.com>
2025-06-28 01:43:35 +0530

    Init Commit

--- src/main/java/PortfolioKK/demo/KasturiPortfolioProjectApplication.java
+++ src/main/java/PortfolioKK/demo/KasturiPortfolioProjectApplication.java
@@ -2,6 +2,8 @@ package PortfolioKK.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class KasturiPortfolioProjectApplication {
@@ -10,4 +12,8 @@ public class KasturiPortfolioProjectApplication {
 		SpringApplication.run(KasturiPortfolioProjectApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
